b9359737653c3a61bf59535a17d99a07b0091c24
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-05 18:28:10 -0400

    Actualizacion de main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(morgan('dev'));
+
   const configService = app.get(ConfigService);
+
+  app.setGlobalPrefix('api');
 
   await app.listen(configService.get('PORT'));
 }
