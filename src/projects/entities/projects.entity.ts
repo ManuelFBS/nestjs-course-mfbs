c0feab5a4fc68ebe90f43f2c46f792418042bf86
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-06 10:36:44 -0400

    Actualizacion de projects.entity.ts

--- src/projects/entities/projects.entity.ts
+++ src/projects/entities/projects.entity.ts
@@ -1,6 +1,7 @@
 import { BaseEntity } from 'src/config/base.entity';
 import { IProject } from '../../interfaces/project.interface';
 import { Column, Entity } from 'typeorm';
+import { UsersProjectsEntity } from 'src/users/entities/usersProjects.entity';
 
 @Entity({ name: 'projects' })
 export class ProjectsEntity extends BaseEntity implements IProject {
@@ -9,4 +10,6 @@ export class ProjectsEntity extends BaseEntity implements IProject {
 
   @Column()
   description: string;
+
+  usersIncludes: UsersProjectsEntity[];
 }
