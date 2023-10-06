d533d125aaf456df18ccaabbc0cfd7e3c6ff5e89
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-06 09:43:53 -0400

    Actualizacion de users.entity.ts

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -1,4 +1,4 @@
-import { BaseEntity } from 'src/config/base.entity';
+import { BaseEntity } from '../../config/base.entity';
 import { IUser } from '../../interfaces/user.interface';
 import { Column, Entity } from 'typeorm';
 
