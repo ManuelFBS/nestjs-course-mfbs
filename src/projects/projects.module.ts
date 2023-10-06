234db60dbd21b397b8ef934bcc50f59d057765d2
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-06 07:25:41 -0400

    Crc. projects.controller, Act projects.module (ts)

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProjectsService } from './services/projects.service';
+import { ProjectsController } from './controller/projects.controller';
 
 @Module({
-  providers: [ProjectsService]
+  providers: [ProjectsService],
+  controllers: [ProjectsController]
 })
 export class ProjectsModule {}
