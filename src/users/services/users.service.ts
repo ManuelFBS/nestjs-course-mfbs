493889f491fa3b0d655c572531227d77a49c3d1e
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-06 22:53:25 -0400

    Actualizacion de users.service.ts

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UsersEntity } from '../entities/users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UsersEntity)
+    private readonly userRepository: Repository<UsersEntity>,
+  ) {}
+}
