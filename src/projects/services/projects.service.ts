c5559aee2691458a745aac6c52c2ece7f8165bea
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-07 11:44:10 -0400

    Actualizacion de projects.service.ts

--- src/projects/services/projects.service.ts
+++ src/projects/services/projects.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { ProjectsEntity } from '../entities/projects.entity';
 
 @Injectable()
-export class ProjectsService {}
+export class ProjectsService {
+  constructor(
+    @InjectRepository(ProjectsEntity)
+    private readonly projectRepository: Repository<ProjectsEntity>,
+  ) {}
+}
