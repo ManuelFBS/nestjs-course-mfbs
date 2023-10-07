67762b76011d8217e865f11ca141a4e9782f365d
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-06 23:22:58 -0400

    Actualizacion de users.controllers.ts

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UserDTO } from '../dto/user.dto';
+import { UsersService } from '../services/users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Post('register')
+  public async createUser(@Body() body: UserDTO) {
+    return await this.userService.createUser(body);
+  }
+
+  @Get('all')
+  public async findAllUsers() {
+    return await this.userService.findUsers();
+  }
+}
