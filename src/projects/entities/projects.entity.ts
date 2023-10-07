59d276614d329814fe4aeed91eca494050f93518
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-06 22:13:27 -0400

    Correccion de path's projects.entity.ts

--- src/projects/entities/projects.entity.ts
+++ src/projects/entities/projects.entity.ts
@@ -1,7 +1,7 @@
-import { BaseEntity } from 'src/config/base.entity';
+import { BaseEntity } from '../../config/base.entity';
 import { IProject } from '../../interfaces/project.interface';
 import { Column, Entity, OneToMany } from 'typeorm';
-import { UsersProjectsEntity } from 'src/users/entities/usersProjects.entity';
+import { UsersProjectsEntity } from '../../users/entities/usersProjects.entity';
 
 @Entity({ name: 'projects' })
 export class ProjectsEntity extends BaseEntity implements IProject {
