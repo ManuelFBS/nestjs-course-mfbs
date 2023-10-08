b9e8fa6477dbd067270fd016d89627c510514900
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-08 03:13:52 -0400

    Act. users.module.ts, auth.(module y service).ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
+import { UsersModule } from '../users/users.module';
 
 @Global()
 @Module({
+  imports: [UsersModule],
   providers: [AuthService],
   controllers: [AuthController],
 })
--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/services/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UsersService) {}
+
+  public async validateUser(username: string, password: string) {}
+}
