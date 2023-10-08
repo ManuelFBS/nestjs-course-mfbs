2a05fc875ff694604acedbc075eed85b05d36c02
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-08 01:28:23 -0400

    Creacion de index.d.ts

---
+++ types/index.d.ts
@@ -0,0 +1,11 @@
+// NodeJS.Processenv
+declare namespace NodeJS {
+  interface ProcessEnv {
+    PORT: number;
+    DB_HOST: string;
+    DB_PORT: number;
+    DB_USER: string;
+    DB_PASSWORD: string;
+    DB_NAME: string;
+  }
+}
