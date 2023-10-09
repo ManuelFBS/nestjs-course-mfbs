55c24bdb09dee75200a6e382100eead22cccc2d8
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-09 11:22:26 -0400

    Actualizacion de auth.module.ts y auth.service.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,11 +2,12 @@ import { Global, Module } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
 import { UsersModule } from '../users/users.module';
+import { UsersService } from '../users/services/users.service';
 
 @Global()
 @Module({
   imports: [UsersModule],
-  providers: [AuthService],
+  providers: [AuthService, UsersService],
   controllers: [AuthController],
 })
 export class AuthModule {}
--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { UsersService } from 'src/users/services/users.service';
+import { UsersService } from '../../users/services/users.service';
 
 @Injectable()
 export class AuthService {
