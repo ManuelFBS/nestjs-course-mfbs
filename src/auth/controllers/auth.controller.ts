bb69d2829aa33bb14cd4f254c25235ceb49594fe
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-09 17:31:57 -0400

    Actualizacion de auth.controler.ts

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,13 +1,13 @@
 import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
-import { AuthBody } from '../interfaces/auth.interface';
+import { AuthDTO } from '../dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  async login(@Body() { username, password }: AuthBody) {
+  async login(@Body() { username, password }: AuthDTO) {
     const userValidate = await this.authService.validateUser(
       username,
       password,
