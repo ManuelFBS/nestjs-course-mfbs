28cb5efd214ab6c15130017398a4c23e333b922f
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-09 12:18:59 -0400

    Actualizacion de auth.service.ts

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/services/users.service';
 
 @Injectable()
 export class AuthService {
-  //
+  constructor(private readonly userService: UsersService) {}
+
+  public async validateUser(username: string, password: string) {}
 }
