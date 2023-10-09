1934313f413eb4204deccaebc61bff30aecf67b0
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-09 17:25:21 -0400

    Actualizacion de auth.controlle.js

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from '../services/auth.service';
+import { AuthBody } from '../interfaces/auth.interface';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  async login(@Body() { username, password }: AuthBody) {
+    const userValidate = await this.authService.validateUser(
+      username,
+      password,
+    );
+
+    if (!userValidate) throw new UnauthorizedException('Data not valid');
+
+    const jwt = await this.authService.generateJWT(userValidate);
+
+    return jwt;
+  }
+}
