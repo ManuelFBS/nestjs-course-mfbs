3907c021d2632b480b5d8de26e11d5949647761b
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-10 00:30:39 -0400

    Actualizacion de auth.interface.ts

--- src/auth/interfaces/auth.interface.ts
+++ src/auth/interfaces/auth.interface.ts
@@ -1,4 +1,5 @@
 import { ROLES } from 'src/constants/roles';
+import { UsersEntity } from 'src/users/entities/users.entity';
 
 export interface PayloadToken {
   sub: string;
@@ -8,4 +9,22 @@ export interface PayloadToken {
 export interface AuthBody {
   username: string;
   password: string;
+}
+
+export interface AuthResponse {
+  accessToken: string;
+  user: UsersEntity;
+}
+
+export interface AuthTokenResult {
+  sub: string;
+  role: string;
+  iat: number;
+  exp: number;
+}
+
+export interface IUseToken {
+  sub: string;
+  role: string;
+  isExpired: boolean;
 }
