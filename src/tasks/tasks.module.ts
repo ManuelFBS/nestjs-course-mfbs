405cc80ece28d5efc953df76644404f6549a5dab
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-12 16:30:27 -0400

    Actualizacion de tasks.module.ts

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './services/tasks.service';
 import { TasksController } from './controllers/tasks.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksEntity } from './entities/tasks.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TasksEntity])],
   providers: [TasksService],
-  controllers: [TasksController]
+  controllers: [TasksController],
 })
 export class TasksModule {}
