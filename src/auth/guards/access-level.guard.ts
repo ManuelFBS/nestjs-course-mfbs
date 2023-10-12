1c3db221fe13cd199b114fa006738c0fbc5cb4d6
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-11 22:40:44 -0400

    Actualizacion de access-level.guard.ts

---
+++ src/auth/guards/access-level.guard.ts
@@ -0,0 +1,67 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
+import {
+  ADMIN_KEY,
+  PUBLIC_KEY,
+  ROLES_KEY,
+} from '../../constants/key-decorators';
+import { ACCESS_LEVEL, ROLES } from '../../constants/roles';
+import { UsersService } from '../../users/services/users.service';
+
+@Injectable()
+export class AccessLevelGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly userService: UsersService,
+  ) {}
+  async canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.get<boolean>(
+      PUBLIC_KEY,
+      context.getHandler(),
+    );
+
+    if (isPublic) return true;
+
+    const roles = this.reflector.get<Array<keyof typeof ROLES>>(
+      ROLES_KEY,
+      context.getHandler(),
+    );
+
+    const accesLevel = this.reflector.get<number>(
+      ACCESS_LEVEL,
+      context.getHandler(),
+    );
+
+    const admin = this.reflector.get<string>(ADMIN_KEY, context.getHandler());
+
+    const req = context.switchToHttp().getRequest<Request>();
+
+    const { roleUser, idUser } = req;
+
+    if (roles === undefined) {
+      if (!admin) {
+        return true;
+      } else if (admin && roleUser === admin) {
+        return true;
+      } else {
+        throw new UnauthorizedException(
+          'No tienes permisos para realizar esta operación...!',
+        );
+      }
+    }
+
+    if (roleUser === ROLES.ADMIN) return true;
+
+    const user = await this.userService.findUserById(parseInt(idUser));
+
+    //const userExistInProject=user.projectsIncludes.find((project)=>project.id===)
+    return true;
+  }
+}
