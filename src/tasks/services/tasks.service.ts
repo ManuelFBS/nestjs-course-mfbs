97dac52243de62104b157ff0e051c63059f7ec38
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-12 16:50:43 -0400

    Actualizacion de tasks.service.ts

--- src/tasks/services/tasks.service.ts
+++ src/tasks/services/tasks.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TasksEntity } from '../entities/tasks.entity';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(
+    @InjectRepository(TasksEntity)
+    private readonly taskRepository: Repository<TasksEntity>,
+  ) {}
+
+  public async createTask(body: any, projectId: number): Promise<TasksEntity> {
+    return await this.taskRepository.save(body);
+  }
+}
