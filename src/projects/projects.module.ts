fa12af3cef7a87f087f3d89f095b2f414da0efd9
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-12 01:07:08 -0400

    Actualizacion de projects.module.ts

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -4,10 +4,11 @@ import { ProjectsController } from './controller/projects.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProjectsEntity } from './entities/projects.entity';
 import { UsersProjectsEntity } from '../users/entities/usersProjects.entity';
+import { UsersService } from '../users/services/users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ProjectsEntity, UsersProjectsEntity])],
-  providers: [ProjectsService],
+  providers: [ProjectsService, UsersService],
   controllers: [ProjectsController],
 })
 export class ProjectsModule {}
