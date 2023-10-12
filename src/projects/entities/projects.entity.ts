6bdcae399409640c1c80a2ed54bcd64eb964caee
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-12 16:22:48 -0400

    Actualizacion de projects.entity.ts

--- src/projects/entities/projects.entity.ts
+++ src/projects/entities/projects.entity.ts
@@ -2,6 +2,7 @@ import { BaseEntity } from '../../config/base.entity';
 import { IProject } from '../../interfaces/project.interface';
 import { Column, Entity, OneToMany } from 'typeorm';
 import { UsersProjectsEntity } from '../../users/entities/usersProjects.entity';
+import { TasksEntity } from '../../tasks/entities/tasks.entity';
 
 @Entity({ name: 'projects' })
 export class ProjectsEntity extends BaseEntity implements IProject {
@@ -13,4 +14,7 @@ export class ProjectsEntity extends BaseEntity implements IProject {
 
   @OneToMany(() => UsersProjectsEntity, (usersProject) => usersProject.project)
   usersIncludes: UsersProjectsEntity[];
+
+  @OneToMany(() => TasksEntity, (tasks) => tasks.project)
+  tasks: TasksEntity[];
 }
