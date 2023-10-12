4f4550c5c27d52bd47ce7fcf9f133a5a9689499b
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-11 22:46:16 -0400

    Actualizacion de projects.controller.ts

--- src/projects/controller/projects.controller.ts
+++ src/projects/controller/projects.controller.ts
@@ -24,21 +24,21 @@ export class ProjectsController {
     return await this.projectService.findProjects();
   }
 
-  @Get(':id')
-  public async findProjectById(@Param('id') id: number) {
+  @Get(':projectId')
+  public async findProjectById(@Param('projectId') id: number) {
     return await this.projectService.findProjectById(id);
   }
 
-  @Put('edit/:id')
+  @Put('edit/:projectId')
   public async updateProject(
     @Body() body: ProjectUpdateDTO,
-    @Param('id') id: number,
+    @Param('projectId') id: number,
   ) {
     return await this.projectService.updateProject(body, id);
   }
 
-  @Delete('delete/:id')
-  public async deleteProject(@Param('id') id: number) {
+  @Delete('delete/:projectId')
+  public async deleteProject(@Param('projectId') id: number) {
     return await this.projectService.deleteProject(id);
   }
 }
