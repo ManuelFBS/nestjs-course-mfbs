786bcfbdd6e829119ad310dfa01f29abf07d9cd4
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-12 22:59:51 -0400

    Creacion de tasks.dto.ts

---
+++ src/tasks/dto/tasks.dto.ts
@@ -0,0 +1,24 @@
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { STATUS_TASK } from '../../constants/status-task';
+import { ProjectDTO } from '../../projects/dto/project.dto';
+
+export class TasksDTO {
+  @IsNotEmpty()
+  @IsString()
+  taskName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  taskDescription: string;
+
+  @IsNotEmpty()
+  @IsEnum(STATUS_TASK)
+  status: STATUS_TASK;
+
+  @IsNotEmpty()
+  @IsString()
+  responsableName: string;
+
+  @IsOptional()
+  project?: ProjectDTO;
+}
