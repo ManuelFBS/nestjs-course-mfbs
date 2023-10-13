a58eb9ef4e45f85159dc7cb04a318ab163436830
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-12 23:12:13 -0400

    Actualizacion de tasks.controller.ts

--- src/tasks/controllers/tasks.controller.ts
+++ src/tasks/controllers/tasks.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
+import { TasksService } from '../services/tasks.service';
+import { TasksDTO } from '../dto/tasks.dto';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private readonly tasksRepository: TasksService) {}
+
+  @Post('create/:projectId')
+  public async createTask(
+    @Body() body: TasksDTO,
+    @Param('projectId') projectId: number,
+  ) {
+    return await this.tasksRepository.createTask(body, projectId);
+  }
+}
