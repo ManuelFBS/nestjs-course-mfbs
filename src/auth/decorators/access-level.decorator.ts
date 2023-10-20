ef485b6b551a8b1a200abe4ce6602d8b8bf765aa
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-20 18:12:36 -0400

    Modificacion de access-level.decorator.ts

--- src/auth/decorators/access-level.decorator.ts
+++ src/auth/decorators/access-level.decorator.ts
@@ -1,5 +1,5 @@
 import { SetMetadata } from '@nestjs/common/decorators';
 import { ACCESS_LEVEL_KEY } from '../../constants/key-decorators';
 
-export const AccessLevel = (level: string) =>
+export const AccessLevel = (level: number) =>
   SetMetadata(ACCESS_LEVEL_KEY, level);
