1b2785f1c7d52718183af1721726ca7ed9777872
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-20 14:00:49 -0400

    Actualizacion de task.controller.ts

--- src/tasks/controllers/tasks.controller.ts
+++ src/tasks/controllers/tasks.controller.ts
@@ -4,12 +4,14 @@ import { TasksDTO } from '../dto/tasks.dto';
 import { AuthGuard } from '../../auth/guards/auth.guard';
 import { RolesGuard } from '../../auth/guards/roles.guard';
 import { AccessLevelGuard } from '../../auth/guards/access-level.guard';
+import { AccessLevel } from '../../auth/decorators/access-level.decorator';
 
 @Controller('tasks')
 @UseGuards(AuthGuard, RolesGuard, AccessLevelGuard)
 export class TasksController {
   constructor(private readonly tasksRepository: TasksService) {}
 
+  @AccessLevel('DEVELOPER')
   @Post('create/:projectId')
   public async createTask(
     @Body() body: TasksDTO,
