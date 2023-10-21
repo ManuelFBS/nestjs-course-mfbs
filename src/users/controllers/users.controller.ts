fc7dfd07be179da1262afba4d4de9c3d215f5ccc
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-21 10:22:45 -0400

    Actualizacion de users.controller.ts

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -12,9 +12,10 @@ import { UserDTO, UserToProjectDTO, UserUpdateDTO } from '../dto/user.dto';
 import { UsersService } from '../services/users.service';
 import { PublicAccess } from '../../auth/decorators/public.decorator';
 import { AuthGuard } from '../../auth/guards/auth.guard';
-import { Roles } from '../../auth/decorators/roles.decorator';
+// import { Roles } from '../../auth/decorators/roles.decorator';
 import { RolesGuard } from '../../auth/guards/roles.guard';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiParam, ApiTags } from '@nestjs/swagger';
+import { AdminAccess } from '../../auth/decorators/admin.decorator';
 
 @ApiTags('Users')
 @Controller('users')
@@ -22,28 +23,39 @@ import { ApiTags } from '@nestjs/swagger';
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
+  @PublicAccess()
   @Post('register')
   public async createUser(@Body() body: UserDTO) {
     return await this.userService.createUser(body);
   }
 
-  @Roles('ADMIN')
+  // @Roles('ADMIN')
+  @AdminAccess()
   @Get('all')
   public async findAllUsers() {
     return await this.userService.findUsers();
   }
 
+  @ApiParam({
+    name: 'id',
+  })
   @PublicAccess()
   @Get(':id')
   public async findUserById(@Param('id') id: number) {
     return await this.userService.findUserById(id);
   }
 
-  @Post('add-to-project')
-  public async addToProject(@Body() body: UserToProjectDTO) {
-    return await this.userService.relationToProject(body);
+  @ApiParam({
+    name: 'projectId',
+  })
+  @Post('add-to-project/:projectId')
+  public async addToProject(@Body() body: UserToProjectDTO, projectId: number) {
+    return await this.userService.relationToProject(body, projectId);
   }
 
+  @ApiParam({
+    name: 'id',
+  })
   @Put('edit/:id')
   public async updateUser(
     @Body() body: UserUpdateDTO,
