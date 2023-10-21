0c62f22da426e11ba26b7f5acd5f34ce67d82f57
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-21 17:13:39 -0400

    Modificacion de projects.controller.ts

--- src/projects/controller/projects.controller.ts
+++ src/projects/controller/projects.controller.ts
@@ -15,8 +15,9 @@ import { RolesGuard } from '../../auth/guards/roles.guard';
 import { AccessLevelGuard } from '../../auth/guards/access-level.guard';
 import { AccessLevel } from '../../auth/decorators/access-level.decorator';
 import { Roles } from '../../auth/decorators/roles.decorator';
-import { ACCESS_LEVEL } from 'src/constants/roles';
-import { ApiTags } from '@nestjs/swagger';
+import { ACCESS_LEVEL } from '../../constants/roles';
+import { ApiParam, ApiTags } from '@nestjs/swagger';
+import { PublicAccess } from '../../auth/decorators/public.decorator';
 
 @ApiTags('Projects')
 @Controller('projects')
@@ -33,16 +34,24 @@ export class ProjectsController {
     return await this.projectService.createProject(body, userId);
   }
 
+  @PublicAccess()
   @Get('all')
   public async findAllProjects() {
     return await this.projectService.findProjects();
   }
 
+  @ApiParam({
+    name: 'projectId',
+  })
+  @PublicAccess()
   @Get(':projectId')
   public async findProjectById(@Param('projectId') id: number) {
     return await this.projectService.findProjectById(id);
   }
 
+  @ApiParam({
+    name: 'projectId',
+  })
   @AccessLevel(ACCESS_LEVEL.OWNER)
   @Put('edit/:projectId')
   public async updateProject(
@@ -52,6 +61,9 @@ export class ProjectsController {
     return await this.projectService.updateProject(body, id);
   }
 
+  @ApiParam({
+    name: 'projectId',
+  })
   @AccessLevel(ACCESS_LEVEL.OWNER)
   @Delete('delete/:projectId')
   public async deleteProject(@Param('projectId') id: number) {
