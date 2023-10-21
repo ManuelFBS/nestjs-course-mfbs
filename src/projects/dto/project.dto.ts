b848cfbd3e9f07c00a2f3f085a1ad6df01448b62
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-21 10:02:29 -0400

    Modificacion de project.dto.ts

--- src/projects/dto/project.dto.ts
+++ src/projects/dto/project.dto.ts
@@ -1,22 +1,20 @@
-import {
-  IsEnum,
-  IsNotEmpty,
-  IsNumber,
-  IsOptional,
-  IsString,
-} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class ProjectDTO {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   description: string;
 }
 
 export class ProjectUpdateDTO {
+  @ApiProperty()
   @IsOptional()
   @IsString()
   description: string;
