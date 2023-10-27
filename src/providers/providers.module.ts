1d609a4588d259dbedcbf28860caec9a3e07eb0d
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-26 23:31:03 -0400

    Actualizacion de providers.module.ts

--- src/providers/providers.module.ts
+++ src/providers/providers.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { HttpService } from './http/http.service';
 
 @Module({
-  providers: [HttpService]
+  providers: [HttpService],
+  exports: [HttpService],
 })
 export class ProvidersModule {}
